92ec048afa40d509381bf622f38e27189448612e
ayoung.yi <yibbiayoung@gmail.com>
2026-04-20 13:29:11 +0900

    api init

--- src/main/kotlin/kr/co/schoolpass/server/Student.kt
+++ src/main/kotlin/kr/co/schoolpass/server/Student.kt
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "students")
